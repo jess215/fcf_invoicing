978e02d7305180d85640b8fe8a6d479e05f2c071
Jessica Nguyen <jessica.stevens2115@gmail.com>
2022-07-27 22:54:50 -0600

    closed invoices json working

--- client/src/Components/ClosedInvoices.js
+++ client/src/Components/ClosedInvoices.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const ClosedInvoices = () => {
+  const [closed, setClosed] = useState([])
+
+  const navigate = useNavigate()
+  let nav = useNavigate()
+
+  useEffect(() => {
+    getClosed()
+  }, [])
+
+  const getClosed = async () => {
+    try {
+      let res = await axios.get('/api/closed')
+      setClosed(res.data)
+    } catch (err) {
+      alert('Error in getting closed invoices')
+    }
+  }
+
   return (
     <div>
       <h2>Closed Invoices</h2>
+      {JSON.stringify(closed)}
     </div>
   )
 }
