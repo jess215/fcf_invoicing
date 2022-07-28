e5a6e51b81b3e20e9fe7ba5798f961a9c0f280d5
Jessica Nguyen <jessica.stevens2115@gmail.com>
2022-07-27 22:17:08 -0600

    json showing for open orders

--- client/src/Pages/Invoices.js
+++ client/src/Pages/Invoices.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const Invoices = () => {
+  const [open, setOpen] = useState([])
+
+  const navigate = useNavigate()
+  let nav = useNavigate()
+
+  useEffect(() => {
+    getOpen()
+  }, [])
+
+  const getOpen = async () => {
+    try {
+      let res = await axios.get('/api/open')
+      setOpen(res.data)
+      console.log('OPEN:', res.data)
+    } catch (err) {
+      alert('Error in getting open invoices')
+    }
+  }
+
   return (
     <div className="container">
       <h1>Open Invoices</h1>
+      {JSON.stringify(open)}
     </div>
   )
 }
