d0bc118af566f2a2e22d6d77f2cf0da04d1788ae
Jessica Nguyen <jessica.stevens2115@gmail.com>
2022-08-01 19:52:11 -0600

    closed table started

--- client/src/Components/ClosedInvoices.js
+++ client/src/Components/ClosedInvoices.js
@@ -1,6 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from '@mui/material'
 
 const ClosedInvoices = () => {
   const [closed, setClosed] = useState([])
@@ -22,10 +31,26 @@ const ClosedInvoices = () => {
   }
 
   return (
-    <div>
-      <h2>Closed Invoices</h2>
-      {JSON.stringify(closed)}
-    </div>
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Company</TableCell>
+            <TableCell>Service Date</TableCell>
+            <TableCell>Invoice Date</TableCell>
+            <TableCell>Paid Date</TableCell>
+            <TableCell>Total</TableCell>
+          </TableRow>
+        </TableHead>
+      </Table>
+    </TableContainer>
+
+    // <div>
+    //   <h2>Closed Invoices</h2>
+    //   {JSON.stringify(closed)}
+    // </div>
   )
 }
 
