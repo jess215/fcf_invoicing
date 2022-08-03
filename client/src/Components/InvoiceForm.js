ddb88fdb3b9be7465a3555c682ed78c4c0905372
Jessica Nguyen <jessica.stevens2115@gmail.com>
2022-08-02 18:53:23 -0600

    invoice form started

--- client/src/Components/InvoiceForm.js
+++ client/src/Components/InvoiceForm.js
@@ -1,10 +1,35 @@
 import React, { useContext, useState } from 'react'
 import axios from 'axios'
 import { useParams, useLocation, useNavigate } from 'react-router-dom'
+import { DataContext } from '../Providers/DataProvider'
 
-const InvoiceForm = () => {
+const InvoiceForm = (props) => {
   const navigate = useNavigate()
   const location = useLocation()
+  const [terms, setTerms] = useState(props.terms || '')
+  const [summary, setSummary] = useState(props.summary || '')
+  const [invoice_date, setInvoiceDate] = useState(props.invoice_date || '')
+  const [paid, setPaid] = useState(props.paid || '')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (props.id) {
+      props.updateInvoice(props.customerId, {
+        id: props.id,
+        terms,
+        summary,
+        invoice_date,
+        paid,
+      })
+      console.log('update here:')
+    } else {
+      props.addInvoice(props.customerId, { terms, summary, invoice_date, paid })
+    }
+    setTerms('')
+    setSummary('')
+    setInvoiceDate('')
+    setPaid('')
+  }
 
   return (
     <div className="container">
